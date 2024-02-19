5bf2e0f3a781e7776d2fd16945700836d175cabf
Matheus Henrique <mamagula@hotmail.com>
2024-02-18 23:59:51 -0300

    Making some adjusts on movie card and page icon

--- src/components/movie poster/movie-poster.jsx
+++ src/components/movie poster/movie-poster.jsx
@@ -24,40 +24,34 @@ export function MoviePoster({ movie, type, posterPath }) {
 
       <Dialog.Portal>
         <Dialog.Overlay className="inset-0 bg-black/60 fixed shadow-lg shadow-white" />
-        <Dialog.Content className="inset-auto z-20 outline-none p-6 fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-3/5 h-[90vh] bg-slate-900 rounded-md space-y-6">
-          <div className="w-full relative flex items-center justify-between">
-            <h1 className="text-slate-50 text-[24px] font-bold">
-              {type === 'tv' ? movie.name : movie.title}
-            </h1>
-
-            <Dialog.Close className="bg-black/50 size-9 rounded-[50%] flex items-center justify-center">
+        <Dialog.Content className="inset-auto z-20 overflow-y-auto outline-none fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-3/5 h-[90vh] bg-slate-900 rounded-md">
+          <div className="w-full z-20 absolute flex p-2 justify-end">
+            <Dialog.Close className="bg-black/50 size-8 rounded-[50%] flex items-center justify-center duration-200 hover:bg-black/100">
               <X />
             </Dialog.Close>
           </div>
-
-          <div className="w-full h-[70%]">
-            {trailer && (
-              <div className="w-full h-full flex items-center justify-center shadow-sm">
+          <div className="w-full">
+            {trailer ? (
+              <div className="w-full h-[65vh] flex items-center justify-center shadow-sm">
+                <div className="absolute w-full h-[65vh] trailer-gradient-top pointer-events-none" />
                 <iframe
-                  className="w-full h-full rounded-md shadow-sm"
+                  className="w-full h-[65vh] rounded-md shadow-sm"
                   src={`https://www.youtube.com/embed/${firstTrailer}?playlist=${trailer}&autoplay=1&controls=0`}
                   allow="accelerometer; fullscreen; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share;"
                 ></iframe>
               </div>
-            )}
-            {firstTrailer && (
-              <div className="w-full h-full flex items-center justify-center shadow-sm">
+            ) : firstTrailer ? (
+              <div className="w-full h-[65vh] flex items-center justify-center shadow-sm">
+                <div className="absolute w-full h-[65vh] trailer-gradient-top pointer-events-none" />
                 <iframe
-                  className="w-full h-full rounded-md shadow-sm"
+                  className="w-full h-[65vh] rounded-md shadow-sm"
                   src={`https://www.youtube.com/embed/${firstTrailer}?autoplay=1&controls=0`}
                   allow="accelerometer; fullscreen; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share;"
                 ></iframe>
               </div>
-            )}
-            {!trailer && !firstTrailer && (
-              <div className="w-full h-full flex items-center justify-center relative shadow-sm">
-                <div className="absolute w-full h-full backdrop-path-gradient-left shadow-sm" />
-                <div className="absolute w-full h-full backdrop-path-gradient-right shadow-sm" />
+            ) : (
+              <div className="w-full h-full flex items-center justify-center relative">
+                <div className="absolute w-full h-full backdrop-path-gradient-top" />
                 <img
                   className="w-full h-full object-cover"
                   src={backdrop_url + movie.backdrop_path}
@@ -65,6 +59,13 @@ export function MoviePoster({ movie, type, posterPath }) {
                 />
               </div>
             )}
+
+            <div className="my-12 px-6 flex flex-col space-y-6">
+              <h1 className="text-slate-50 text-[28px] font-bold">
+                {type === 'tv' ? movie.name : movie.title}
+              </h1>
+              <h1 className="text-lg font-semibold">Temporadas</h1>
+            </div>
           </div>
         </Dialog.Content>
       </Dialog.Portal>
